01e42a08a032de99676e9739b25a04032fb0d23e
emrope1 <emrope@hanmail.net>
2017-06-27 16:54:43 +0900

    4
    4

---
+++ PumpKing/Assets/Scripts_LKH/CoolTimeOBJClose.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolTimeOBJClose : MonoBehaviour
+{
+
+    public GameObject bOomOff;
+    float cTime;
+    public float FalseTime;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        cTime += Time.deltaTime;
+
+        if (cTime > FalseTime)
+        {
+
+            bOomOff.SetActive(false);
+            cTime = 0;
+
+
+        }
+
+
+    }
+}
---
+++ PumpKing/Assets/Scripts_LKH/CoolTimeYounSubb.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolTimeYounSubb : MonoBehaviour
+{
+    public GameObject bOomEndControl;
+    public GameObject bOomBotton;
+
+    int count = 5;
+    public bool eTime = false;
+    public float cOolTime = 0;
+    public float EndCoolTime;
+
+    void Update()
+    {
+        //Debug.Log(Time.timeScale);
+        if (eTime == true)
+        {
+            bOomBotton.SetActive(false);
+            cOolTime += Time.deltaTime;
+            //Debug.Log("2");
+            if (cOolTime > EndCoolTime)
+            {
+                bOomBotton.SetActive(true);
+                cOolTime = 0;
+                eTime = false;
+                //Debug.Log("2_Q");
+            }
+        }
+    }
+
+    public void BooMEnd()
+    {
+        count--;
+        //Debug.Log("1");
+        eTime = true;
+        if (count == 0)
+        {
+            bOomEndControl.SetActive(false);
+            //Debug.Log("1_Q");          
+        }
+    }
+}
